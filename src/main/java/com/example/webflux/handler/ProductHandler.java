56ecd48616b60c9a2fbface2a8638464b127be10
ppsseo18 <ppsseo18@gmail.com>
2018-10-25 17:09:08 +0900

    Add product get, put, delete and proxy router

--- src/main/java/com/example/webflux/handler/ProductHandler.java
+++ src/main/java/com/example/webflux/handler/ProductHandler.java
@@ -4,8 +4,11 @@ import com.example.webflux.model.Product;
 import com.example.webflux.model.ResponseBody;
 import com.example.webflux.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,6 +20,20 @@ public class ProductHandler {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    public Mono<ServerResponse> get(ServerRequest request) {
+        Integer productId = Integer.parseInt(request.pathVariable("productId"));
+
+        return ServerResponse
+                .status(HttpStatus.CREATED)
+                .body(productRepository.findById(productId)
+                                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found")))
+                                .map(product -> ResponseBody.ok(request.path(), product))
+                        , ResponseBody.class);
+    }
+
     public Mono<ServerResponse> getAll(ServerRequest request) {
         return ServerResponse
                 .status(HttpStatus.CREATED)
@@ -26,7 +43,7 @@ public class ProductHandler {
                         , ResponseBody.class);
     }
 
-    public Mono<ServerResponse> create(ServerRequest request) {
+    public Mono<ServerResponse> post(ServerRequest request) {
         return ServerResponse
                 .status(HttpStatus.CREATED)
                 .body(request.bodyToMono(Product.class)
@@ -38,4 +55,91 @@ public class ProductHandler {
                         , ResponseBody.class);
     }
 
+    public Mono<ServerResponse> put(ServerRequest request) {
+        return ServerResponse
+                .status(HttpStatus.CREATED)
+                .body(request.bodyToMono(Product.class)
+                                .flatMap(requestProduct -> productRepository.findById(requestProduct.getId())
+                                        .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.CONFLICT, "Product Already Exist")))
+                                        .flatMap(product -> productRepository.save(requestProduct))
+                                )
+                                .map(product -> ResponseBody.ok(request.path(), product))
+                        , ResponseBody.class);
+    }
+
+    public Mono<ServerResponse> delete(ServerRequest request) {
+        Integer productId = Integer.parseInt(request.pathVariable("productId"));
+
+        return ServerResponse
+                .status(HttpStatus.CREATED)
+                .body(productRepository.findById(productId)
+                                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found")))
+                                .flatMap(product -> productRepository.delete(product))
+                                .then(Mono.just(ResponseBody.ok(request.path(), productId))), ResponseBody.class);
+    }
+
+
+    public Mono<ServerResponse> proxyGet(ServerRequest request) {
+        return ServerResponse.ok().body(webClientBuilder
+                .build()
+                .get()
+                .uri("/product/{productId}", request.pathVariable("productId"))
+                .retrieve()
+                .onStatus(HttpStatus::isError, error -> Mono.error(new ResponseStatusException(error.statusCode())))
+                .bodyToMono(new ParameterizedTypeReference<ResponseBody<Product>>() {})
+                .map(userResponseBody -> ResponseBody.ok(request.path(), userResponseBody.getData())), ResponseBody.class);
+    }
+
+    public Mono<ServerResponse> proxyGetAll(ServerRequest request) {
+        return ServerResponse.ok().body(webClientBuilder
+                .build()
+                .get()
+                .uri("/product")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .retrieve()
+                .onStatus(HttpStatus::isError, error -> Mono.error(new ResponseStatusException(error.statusCode())))
+                .bodyToFlux(new ParameterizedTypeReference<ResponseBody<Product>>() {})
+                .map(userResponseBody -> ResponseBody.ok(request.path(), userResponseBody.getData())), ResponseBody.class);
+    }
+
+    public Mono<ServerResponse> proxyPost(ServerRequest request) {
+        return ServerResponse.ok().body(
+                request.bodyToMono(Product.class)
+                        .flatMap(requestBody -> webClientBuilder
+                                .build()
+                                .post()
+                                .uri("/product")
+                                .syncBody(requestBody)
+                                .retrieve()
+                                .onStatus(HttpStatus::isError, error -> Mono.error(new ResponseStatusException(error.statusCode())))
+                                .bodyToMono(new ParameterizedTypeReference<ResponseBody<Product>>() {})
+                                .map(userResponseBody -> ResponseBody.created(request.path(), userResponseBody.getData())))
+                , ResponseBody.class);
+    }
+
+    public Mono<ServerResponse> proxyPut(ServerRequest request) {
+        return ServerResponse.ok().body(
+                request.bodyToMono(Product.class)
+                        .flatMap(requestBody -> webClientBuilder
+                                .build()
+                                .put()
+                                .uri("/user")
+                                .syncBody(requestBody)
+                                .retrieve()
+                                .onStatus(HttpStatus::isError, error -> Mono.error(new ResponseStatusException(error.statusCode())))
+                                .bodyToMono(new ParameterizedTypeReference<ResponseBody<Product>>() {})
+                                .map(userResponseBody -> ResponseBody.ok(request.path(), userResponseBody.getData())))
+                , ResponseBody.class);
+    }
+
+    public Mono<ServerResponse> proxyDelete(ServerRequest request) {
+        return ServerResponse.ok().body(webClientBuilder
+                .build()
+                .delete()
+                .uri("/product/{productId}", request.pathVariable("productId"))
+                .retrieve()
+                .onStatus(HttpStatus::isError, error -> Mono.error(new ResponseStatusException(error.statusCode())))
+                .bodyToMono(new ParameterizedTypeReference<ResponseBody<Integer>>() {})
+                .map(responseBody -> ResponseBody.ok(request.path(), responseBody.getData())), ResponseBody.class);
+    }
 }
--- src/main/java/com/example/webflux/router/ProductRouter.java
+++ src/main/java/com/example/webflux/router/ProductRouter.java
@@ -1,5 +1,6 @@
 package com.example.webflux.router;
 
+import com.example.webflux.filter.BasicAuthenticationFilter;
 import com.example.webflux.handler.ProductHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +15,23 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class ProductRouter {
 
     @Autowired
+    private BasicAuthenticationFilter basicAuthenticationFilter;
+
+    @Autowired
     private ProductHandler productHandler;
 
     @Bean
     public RouterFunction<ServerResponse> productRoutes() {
-        return RouterFunctions.route(RequestPredicates.POST("/product"), productHandler::create)
-                .andRoute(RequestPredicates.GET("/product").and(RequestPredicates.accept(MediaType.APPLICATION_JSON).or(RequestPredicates.accept(MediaType.TEXT_EVENT_STREAM))), productHandler::getAll);
+        return RouterFunctions.route(RequestPredicates.GET("/product/{productId}"), productHandler::get)
+                .andRoute(RequestPredicates.GET("/product").and(RequestPredicates.accept(MediaType.APPLICATION_JSON).or(RequestPredicates.accept(MediaType.TEXT_EVENT_STREAM))), productHandler::getAll)
+                .andRoute(RequestPredicates.POST("/product"), productHandler::post)
+                .andRoute(RequestPredicates.PUT("/product").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)).and(RequestPredicates.contentType(MediaType.APPLICATION_JSON)), productHandler::put)
+                .andRoute(RequestPredicates.DELETE("/product/{productId}"), productHandler::delete)
+                .filter(basicAuthenticationFilter::filter)
+                .andRoute(RequestPredicates.GET("/proxy/product/{productId}"), productHandler::proxyGet)
+                .andRoute(RequestPredicates.GET("/proxy/product").and(RequestPredicates.accept(MediaType.APPLICATION_JSON).or(RequestPredicates.accept(MediaType.TEXT_EVENT_STREAM))), productHandler::proxyGetAll)
+                .andRoute(RequestPredicates.POST("/proxy/product"), productHandler::proxyPost)
+                .andRoute(RequestPredicates.PUT("/proxy/product").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)).and(RequestPredicates.contentType(MediaType.APPLICATION_JSON)), productHandler::proxyPut)
+                .andRoute(RequestPredicates.DELETE("/proxy/product/{productId}"), productHandler::proxyDelete);
     }
 }
