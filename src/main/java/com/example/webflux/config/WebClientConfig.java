4d6a1d2474cd688c8887f0dcd13f7da4b3ec77f1
ppsseo18 <ppsseo18@gmail.com>
2018-10-25 15:59:57 +0900

    Add timeout option to WebClient

--- src/main/java/com/example/webflux/config/WebClientConfig.java
+++ src/main/java/com/example/webflux/config/WebClientConfig.java
@@ -1,19 +1,34 @@
 package com.example.webflux.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient.Builder getWebclientBuilder() {
+        ReactorClientHttpConnector httpConnector =new ReactorClientHttpConnector(options -> options
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
+                .compression(true)
+                .afterNettyContextInit(ctx -> {
+                    ctx.addHandlerLast(new ReadTimeoutHandler(3000, TimeUnit.MILLISECONDS));
+                    ctx.addHandlerLast(new WriteTimeoutHandler(3000, TimeUnit.MILLISECONDS));
+                }));
+
         return WebClient.builder()
+                .clientConnector(httpConnector)
                 .baseUrl("http://localhost:8000")
                 .defaultHeader(HttpHeaders.USER_AGENT, "webflux example")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
